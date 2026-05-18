7d40232006423eff1820662c5f228413a8576464
AlejandroFerreira1 <alejandro19812007@gmail.com>
2026-05-18 08:32:03 -0300

    primeiro commit

---
+++ logica.py
@@ -0,0 +1,7 @@
+nome= float(input('qual é seu nome? '))
+peso= float(input('informe qual seu peso: '))
+altura= float(input('informe qual é seu peso: '))
+
+imc = peso / (altura ** 2)
+
+print(nome," seu IMC é: ", imc)
