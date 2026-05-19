3a9e68735ad360c8057a537ca90458ba733f4374
AlejandroFerreira1 <alejandro19812007@gmail.com>
2026-05-19 07:24:37 -0300

    entrada de paciente

--- logica.py
+++ logica.py
@@ -1,2 +1,5 @@
-nome=input("Digite seu nome: ")
-print("Olá, " + nome + "! Seja bem-vindo(a) ao mundo da programação em Python!")
+nome_do_paciente = "João"
+idade_do_paciente = 30
+sexo_do_paciente = "Masculino"
+data_de_entrada = "2024-06-01"
+é_um_novo_paciente = True
